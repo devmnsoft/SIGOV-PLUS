66570e4ec6a1425b3c310b475f45aac5d985131e
MNSOFT <137189378+devmnsoft@users.noreply.github.com>
2026-06-10 07:50:10 -0300

    fix: adjust post evolution test compilation

--- tests/Sigov.UnitTests/TestRepoPath.cs
+++ tests/Sigov.UnitTests/TestRepoPath.cs
@@ -2,14 +2,23 @@ namespace Sigov.Testing;
 
 public static class TestRepoPath
 {
+    public static string Root
+    {
+        get
+        {
+            var directory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            while (directory is not null && !File.Exists(Path.Combine(directory.FullName, "sigov.sln")))
+            {
+                directory = directory.Parent;
+            }
+
+            return directory?.FullName ?? Directory.GetCurrentDirectory();
+        }
+    }
+
     public static string Get(string relativePath)
     {
-        var directory = new DirectoryInfo(AppContext.BaseDirectory);
-        while (directory is not null && !File.Exists(Path.Combine(directory.FullName, "sigov.sln")))
-        {
-            directory = directory.Parent;
-        }
-
-        return Path.Combine(directory?.FullName ?? Directory.GetCurrentDirectory(), relativePath);
+        return Path.Combine(Root, relativePath);
     }
 }
